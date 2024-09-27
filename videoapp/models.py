944567c8aedb9014d557a6c3ccd717d8fe3ff58d
bsb78OBO <vladimirsobol@MacBook-Pro-Vladimir-2.local>
2024-09-27 19:47:05 +0300

    создана модель

--- videoapp/models.py
+++ videoapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class VideoRequest(models.Model):
+    message = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
